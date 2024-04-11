0e221897d020fe4d9585c3475800e36ad5cc2b6a
odenra <ilkerkaraoden@gmail.com>
2024-04-11 23:35:58 +0300

    jobs list with the better components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Hero from "./components/Hero"
 import HomeCards from "./components/HomeCards"
 import JobListings from "./components/JobListings"
 import Navbar from "./components/Navbar"
+import ViewAllJobs from "./components/ViewAllJobs"
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <HomeCards />
       <JobListings />
+      <ViewAllJobs />
     </>
   )
 }
