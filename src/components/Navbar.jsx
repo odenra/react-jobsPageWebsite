3dfc67b7f1b007570cc5e65e90c5fd94341ba891
odenra <ilkerkaraoden@gmail.com>
2024-04-12 12:07:42 +0300

    active links done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+
+  const linkClass = ({ isActive }) =>
+    isActive
+      ? 'bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
+      : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2';
+
   return (
     <>
       <nav className="bg-indigo-700 border-b border-indigo-500">
@@ -12,29 +18,29 @@ const Navbar = () => {
 
             <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
                 
-              <Link className="flex flex-shrink-0 items-center mr-4" to="/">
+              <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
 
                 <span className="hidden md:block text-white text-2xl font-bold ml-2">
                   React Jobs
                 </span>
 
-              </Link>
+              </NavLink>
               
               <div className="md:ml-auto">
 
                 <div className="flex space-x-2">
 
-                  <Link to="/" className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">
+                  <NavLink to="/" className={linkClass}>
                     Home
-                  </Link>
+                  </NavLink>
 
-                  <Link to="/jobs" className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">
+                  <NavLink to="/jobs" className={linkClass}>
                     Jobs
-                  </Link>
+                  </NavLink>
 
-                  <Link to="/add-job" className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">
+                  <NavLink to="/add-job" className={linkClass}>
                     Add Job
-                  </Link>
+                  </NavLink>
 
                 </div>
               </div>
