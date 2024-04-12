3de3d4f40c7c6d3ce03a1d3a9194c7855e8d3445
odenra <ilkerkaraoden@gmail.com>
2024-04-12 17:38:50 +0300

    job page with data loader react

--- src/components/JobListining.jsx
+++ src/components/JobListining.jsx
@@ -40,8 +40,9 @@ const JobListining = ({ job }) => {
             </div>
 
             <Link
-              to="{`/job/${job.id}`}"
-              className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+              // eslint-disable-next-line react/prop-types
+              to={`/jobs/${job.id}`}
+              className='h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm'
             >
               Read More
             </Link>
