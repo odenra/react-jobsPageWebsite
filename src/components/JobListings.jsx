d1a120220f34473dcc50658f2a997b964a49bcc7
odenra <ilkerkaraoden@gmail.com>
2024-04-12 14:52:23 +0300

    proxying

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,50 +1,49 @@
-import { useState, useEffect } from 'react';
-import JobListining from '../components/JobListining';
+import { useState, useEffect } from "react";
+import JobListining from "../components/JobListining";
+import Spinner from "./Spinner";
 
-const JobListings = ({  isHome = false }) => {
+const JobListings = ({ isHome = false }) => {
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    const [jobs, setJobs] = useState([]);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-      const fetchJobs = async () => {
-        try {
-          const res = await fetch('http://localhost:5000/jobs');
-          const data = await res.json();
-          setJobs(data);
-        } catch (error) {
-          console.log(error, 'error')
-        } finally {
-          setLoading(false);
-        }
-        
+  useEffect(() => {
+    const fetchJobs = async () => {
+      const apiUrl = isHome 
+        ? '/api/jobs?_limit=3' 
+        : '/api/jobs';
+      try {
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.log(error, "error");
+      } finally {
+        setLoading(false);
       }
+    };
 
-      fetchJobs();
-    }, [])
+    fetchJobs();
+  }, []);
 
   return (
     <>
       <section className="bg-blue-50 px-4 py-10">
-
         <div className="container-xl lg:container m-auto">
-
           <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-            {isHome ? 'Recent Jobs' : 'Browse All Jobs'}
+            {isHome ? "Recent Jobs" : "Browse All Jobs"}
           </h2>
 
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-
-            {jobs.map ((job) => (
-                
-                <JobListining key={job.id} job={ job }/>
-
-            ))}
-
+          
+            {loading ? (
+              <Spinner loading={loading} />
+            ) : (
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                {jobs.map((job) => (
+                  <JobListining key={job.id} job={job} />
+                ))}
+              </div>
+            )}
           </div>
-
-        </div>
-
       </section>
     </>
   );
