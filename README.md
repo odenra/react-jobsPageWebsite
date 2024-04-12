253fb5a69d982b3dc6e192ec64aa9b754ee10be4
odenra <ilkerkaraoden@gmail.com>
2024-04-12 12:59:20 +0300

    .

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ This is the jobs listing project from the Crash Course
 
 This project uses JSON-Server for a mock backend.
 
+This project uses Tailwind for styling
--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -12,7 +12,7 @@ const JobListings = ({  isHome = false }) => {
         <div className="container-xl lg:container m-auto">
 
           <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-            Browse Jobs
+            {isHome ? 'Recent Jobs' : 'Browse All Jobs'}
           </h2>
 
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
