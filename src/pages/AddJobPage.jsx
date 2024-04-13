89960f2a899052b02d7a4b2a17e0ae7d15b502c5
odenra <ilkerkaraoden@gmail.com>
2024-04-13 13:42:44 +0300

    toastify added

--- src/pages/AddJobPage.jsx
+++ src/pages/AddJobPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const AddJobPage = ({ addJobSubmit }) => {
 
@@ -33,6 +34,8 @@ const AddJobPage = ({ addJobSubmit }) => {
         };
 
         addJobSubmit(newJob);
+
+        toast.success('Job Added..')
 
         return navigate('/jobs');
     }
